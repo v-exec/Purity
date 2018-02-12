abf368915d591a35273d0ceb73f429a9bc947f69
V <reducedprocess@gmail.com>
2018-02-11 23:30:01 -0500

    add svg support

--- assets/parser.php
+++ assets/parser.php
@@ -275,6 +275,7 @@ class Parser {
 		$image = $image.'.png';
 		if (!file_exists($image)) $image = substr($image, 0, strlen($image) - 4).'.jpg';
 		if (!file_exists($image)) $image = substr($image, 0, strlen($image) - 4).'.gif';
+		if (!file_exists($image)) $image = substr($image, 0, strlen($image) - 4).'.svg';
 
 		$image = str_replace(' ', '%20', $image);
 
